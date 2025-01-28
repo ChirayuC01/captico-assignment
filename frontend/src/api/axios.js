8cbfc24c42b440d8450f49242097f72fda9606b6
Chriayu01 <chirayuchawande01@gmail.com>
2025-01-29 01:51:18 +0530

    updated frontend with live api

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api", // Backend base URL
+    // baseURL: "http://localhost:5000/api", // Local Backend base URL
+    baseURL: "https://captico-assignment-api.vercel.app/api", // Live Backend base URL
 });
 
 // Add a token to each request if available
