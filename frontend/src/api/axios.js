e336567ccccfa7d4b94b3c21408dab1360afc970
Chriayu01 <chirayuchawande01@gmail.com>
2025-01-29 17:21:29 +0530

    bug fix

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
     // baseURL: "http://localhost:5000/api/", // Local Backend base URL
-    baseURL: "https://captico-assignment-api.vercel.app/api/", // Live Backend base URL
+    baseURL: "https://captico-assignment-api.vercel.app/api", // Live Backend base URL
 });
 
 // Add a token to each request if available
