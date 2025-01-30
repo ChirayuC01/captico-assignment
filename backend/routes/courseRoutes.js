e3c65357e05dcb166d96003ecaf21e0615e2f975
Chriayu01 <chirayuchawande01@gmail.com>
2025-01-31 01:52:43 +0530

    added middleware

--- backend/routes/courseRoutes.js
+++ backend/routes/courseRoutes.js
@@ -8,16 +8,18 @@ const {
     bulkUploadCourses
 } = require("../controllers/courseController");
 
+const { protect } = require("../middleware/authMiddleware");
+
 const router = express.Router();
 
 // Plural for collections
 router.get("/courses", getCourses); // Fetch all courses
-router.post("/courses", createCourse); // Create a new course
-router.post("/courses/bulk-upload", bulkUploadCourses); // Create multiple new course
+router.post("/courses", protect, createCourse); // Create a new course
+router.post("/courses/bulk-upload", protect, bulkUploadCourses); // Create multiple new course
 
 // Singular for individual entities
 router.get("/course/:id", getCourseById); // Fetch a course by ID
-router.put("/course/:id", updateCourse); // Update a course by ID
-router.delete("/course/:id", deleteCourse); // Delete a course by ID
+router.put("/course/:id", protect, updateCourse); // Update a course by ID
+router.delete("/course/:id", protect, deleteCourse); // Delete a course by ID
 
 module.exports = router;
